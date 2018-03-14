8a29bcc8677e46f1185dfcb9233e9f837a926539
suhas456 <36154495+suhas456@users.noreply.github.com>
2018-03-14 20:16:43 +0530

    Add files via upload

---
+++ filemanipulation.py
@@ -0,0 +1,13 @@
+#file manipulation
+r=open('vp.txt','w')
+r.write('Hello!!')
+r=open('vp.txt','r')
+r.read()
+r=open('vp.txt','a')
+r.write('This is Vijayakumar')
+r=open('vp.txt','r')
+r.read()
+print("Done")
+
+
+
